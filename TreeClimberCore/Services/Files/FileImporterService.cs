778fd1abd81288a728a6c13668c75f7fa414c570
Jason O <jaywoc12@gmail.com>
2025-02-13 14:46:39 -0500

    added services

---
+++ TreeClimberCore/Services/Files/FileImporterService.cs
@@ -0,0 +1,28 @@
+﻿
+
+using Microsoft.AspNetCore.Components.Forms;
+using Newtonsoft.Json.Linq;
+
+namespace TreeClimberCore.Services.Files
+{
+	public class FileImporterService
+	{
+		public static async Task<JObject> ConvertFileContentsToJObjectAsync(IBrowserFile file)
+		{
+			string fileContents = await ConvertFileContentsToStringAsync(file);
+
+			return JObject.Parse(fileContents);
+		}
+
+		protected static async Task<string> ConvertFileContentsToStringAsync(IBrowserFile file)
+		{
+			string fileContents;
+			using (var stream = file.OpenReadStream())
+			using (var reader = new StreamReader(stream))
+			{
+				fileContents = await reader.ReadToEndAsync();
+			}
+			return fileContents;
+		}
+	}
+}
