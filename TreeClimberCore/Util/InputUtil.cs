0fad1d591829db381b6e602c3c1212343e3fbc22
Jason O <jaywoc12@gmail.com>
2025-02-14 15:24:04 -0500

    added util classes

---
+++ TreeClimberCore/Util/InputUtil.cs
@@ -0,0 +1,33 @@
+﻿namespace TreeClimberCore.Util
+{
+    public class InputUtil
+    {
+        public const string VALUES = "values";
+        public const string NOTSET = "notset";
+
+        public const string STRING = "string";
+        public const string INTEGER = "integer";
+        public const string FLOAT = "float";
+        public const string BOOL = "bool";
+        public const string DATE = "timestamp";
+        public const string CUSTOM = "custom";
+
+        public const string TEXT = "text";
+        public const string NUMERIC = "numeric";
+        public const string COMBOBOX = "combobox";
+        public const string DATEPICK = "datepick";
+        public const string DATETIMEPICK = "datetimepick";
+        public const string TABLE = "table";
+        public const string VALUE_LIST = "value_list";
+
+		public const string CURRENT = "current";
+		public const string CURRENT_USER = "current_user";
+        public const string CURRENT_USER_NAME = "current_user_name";
+
+        public const string LONG = "long";
+        public const string MULTILINE = "multiline";
+
+        public const string DATE_FORMAT = "yyyy-MM-dd";
+        public const string TIMESTAMP_FORMAT = "yyyy-M-dTHH:mm:ssZ";
+    }
+}
---
+++ TreeClimberCore/Util/ResponseUtil.cs
@@ -0,0 +1,10 @@
+﻿namespace TreeClimberCore.Util
+{
+	public static class ResponseUtil
+	{
+		public const int OK = 200;
+		public const int CREATED = 201;
+
+		public const int INTERNAL_ERROR = 482;
+	}
+}
