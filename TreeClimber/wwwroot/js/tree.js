7d900a28404ee33df68d9f45981dfb4b5307ac92
Jason O <jaywoc12@gmail.com>
2025-02-27 01:08:16 -0500

    added json file with functions for expanding and collapsing all climb elements

---
+++ TreeClimber/wwwroot/js/tree.js
@@ -0,0 +1,18 @@
+﻿/*TREE FUNCTIONS variable will store our functions instead of putting them in the window */
+TREE_FUNCTIONS = {
+    expandAll: function () {
+        var x = document.getElementsByTagName("details");
+        var i;
+        for (i = 0; i < x.length; i++) {
+            x[i].setAttribute("open", "true");
+        }
+    },
+
+    collapseAll: function () {
+        var x = document.getElementsByTagName("details");
+        var i;
+        for (i = 0; i < x.length; i++) {
+            x[i].removeAttribute("open");
+        }
+    }
+}
